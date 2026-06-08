b3368d358cb59ef80d72d1b2bf4183e41f0c81c0
Rahim <mac@MacBook-Air-Rahim.local>
2026-06-08 13:58:14 +0200

    set landing as home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import { redirect } from 'next/navigation'
-
 export default function Home() {
   return (
-    <main style={{fontFamily:'-apple-system,sans-serif',background:'#fff',minHeight:'100vh'}}>
-      <iframe src="/landing.html" style={{width:'100%',height:'100vh',border:'none'}} />
-    </main>
+    <iframe src="/landing.html" style={{width:'100%',height:'100vh',border:'none',display:'block'}} />
   )
 }
